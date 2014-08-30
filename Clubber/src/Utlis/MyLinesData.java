9ed09b82085b48c1d35d1cccec8a57a85046e613
Gil401 <Gil401@gmail.com>
2014-08-30 20:44:59 +0300

    OfferPerAuction

---
+++ Clubber/src/Utlis/MyLinesData.java
@@ -0,0 +1,22 @@
+package Utlis;
+
+import java.util.List;
+
+public class MyLinesData {
+	
+	private List<IdWithName> lines;
+	private List<IdWithName> status;
+	public List<IdWithName> getLines() {
+		return lines;
+	}
+	public void setLines(List<IdWithName> lines) {
+		this.lines = lines;
+	}
+	public List<IdWithName> getStatus() {
+		return status;
+	}
+	public void setStatus(List<IdWithName> status) {
+		this.status = status;
+	}
+
+}
---
+++ Clubber/src/Utlis/OfferPerAuction.java
@@ -0,0 +1,24 @@
+package Utlis;
+
+import ClubberLogic.AuctionData;
+import ClubberLogic.OfferData;
+
+public class OfferPerAuction {
+	private AuctionData m_Auction;
+	private OfferData m_OfferData;
+	
+	public AuctionData getM_Auction() {
+		return m_Auction;
+	}
+	public void setM_Auction(AuctionData m_Auction) {
+		this.m_Auction = m_Auction;
+	}
+	public OfferData getM_OfferData() {
+		return m_OfferData;
+	}
+	public void setM_OfferData(OfferData m_OfferData) {
+		this.m_OfferData = m_OfferData;
+	}
+	
+	
+}
